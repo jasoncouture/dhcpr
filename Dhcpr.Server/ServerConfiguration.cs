a5cf4f07e074ea6267d900e62713fed6faa4f6d8
Jason Couture <jasonc@alertr.info>
2023-08-31 18:41:37 -0400

    Move DNS configuration to appsettings.Development.json

--- Dhcpr.Server/ServerConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Dhcpr.Server;
-
-public sealed class ServerConfiguration
-{
-    public string DnsConfigurationFile { get; set; } = "dns.json";
-    public string DhcpConfigurationFile { get; set; } = "dhcp.json";
-}

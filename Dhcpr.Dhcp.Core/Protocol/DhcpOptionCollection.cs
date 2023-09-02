c95a168bec21b3d084acaeed8fdf83ee39538e16
Jason Couture <jasonc@alertr.info>
2023-09-02 12:44:24 -0400

    DHCP message parsing (Technically, bootp+options, when magic cookie is present).

---
+++ Dhcpr.Dhcp.Core/Protocol/DhcpOptionCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace Dhcpr.Dhcp.Core.Protocol;
+
+public sealed record DhcpOptionCollection(ImmutableArray<DhcpOption> Options) : IEnumerable<DhcpOption>
+{
+    private int? _length;
+    public int Length => _length ??= Options.Select(i => i.Length).DefaultIfEmpty(0).Sum(i => i);
+
+    public IEnumerator<DhcpOption> GetEnumerator()
+    {
+        return Options.AsEnumerable().GetEnumerator();
+    }
+
+    public override string ToString()
+    {
+        return string.Concat($"{{ {Environment.NewLine}", string.Join($",{Environment.NewLine}", Options.Select(i => $"\t{i.ToString()}")), $"{Environment.NewLine}}}");
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Dhcpr.Dhcp.Core/Protocol/HardwareAddress.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+
+namespace Dhcpr.Dhcp.Core.Protocol;
+
+public sealed record HardwareAddress(ImmutableArray<byte> Bytes, int Length)
+{
+    public override string ToString()
+    {
+        var slicedBytes = Bytes[..Length].ToArray();
+        return BitConverter.ToString(slicedBytes).Replace('-', ':').ToUpper();
+    }
+}

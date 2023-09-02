2f6cc77eeacbacb6c154318b51ad064bb8c79597
Jason Couture <jasonc@alertr.info>
2023-09-01 20:48:57 -0400

    Move types to their own files

--- Dhcpr.Dhcp.Core/DhcpConfiguration.cs
+++ Dhcpr.Dhcp.Core/DhcpConfiguration.cs
@@ -1,43 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Net.NetworkInformation;
-
-using Dhcpr.Core;
-
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Dhcpr.Core;
 
 namespace Dhcpr.Dhcp.Core;
 
-[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
-public sealed class SubnetConfiguration : IValidateSelf
-{
-    public required string CIDR { get; set; }
-    public int TimeToLiveSeconds { get; set; }
-    public bool Enabled { get; set; }
-    public bool Validate()
-    {
-        if (TimeToLiveSeconds < 300) return false;
-        if (!CIDR.TryParseClasslessInterDomainRouting(out var cidrAddress, out var cidrNetmask))
-            return false;
-        try
-        {
-            var foundNetworkAddress = NetworkInterface.GetAllNetworkInterfaces()
-                .Any(i =>
-                    i.GetIPProperties()
-                        .UnicastAddresses
-                        .Any(x => x.Address.IsInNetwork(cidrAddress, cidrNetmask)
-                        ));
-            if (!foundNetworkAddress) return false;
-        }
-        catch
-        {
-            // Assume it's valid, and we just don't have permission.
-            // We'll find out soon enough when we try to bind interfaces.
-        }
-
-        return true;
-    }
-}
 public sealed class DhcpConfiguration : IValidateSelf
 {
     public required SubnetConfiguration[] Subnets { get; set; }
@@ -45,17 +9,5 @@ public sealed class DhcpConfiguration : IValidateSelf
     public bool Validate()
     {
         return Subnets.All(subnet => subnet.Validate());
-    }
-}
-
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection AddDhcp(this IServiceCollection services, IConfiguration configuration)
-    {
-        // TODO: Add DHCP components and configuration.
-        
-        services.Configure<DhcpConfiguration>(configuration);
-        return services;
     }
 }

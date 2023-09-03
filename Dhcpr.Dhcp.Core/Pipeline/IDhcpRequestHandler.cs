ca2a207237278eda16b49c5eeac9c8a93f178330
Jason Couture <jasonc@alertr.info>
2023-09-03 16:33:14 -0400

    Starting DHCP server development, now that protocol is ironed out

---
+++ Dhcpr.Dhcp.Core/Pipeline/IDhcpRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+using Dhcpr.Dhcp.Core.Protocol;
+
+namespace Dhcpr.Dhcp.Core.Pipeline;
+
+public interface IDhcpRequestHandler
+{
+    int Order => 0;
+    ValueTask HandleDhcpRequest(DhcpRequestContext context, CancellationToken cancellationToken);
+}
+
+public class DhcpRequestContext
+{
+    public required DhcpNetworkInformation NetworkInformation { get; init; }
+    public required DhcpMessage Message { get; init; }
+    public DhcpMessage? Response { get; init; }
+}
+
+public record DhcpNetworkInformation(
+    IPAddress LocalAddress,
+    IPAddress NetworkMask,
+    IPAddress BroadcastAddress,
+    int InterfaceIndex
+);

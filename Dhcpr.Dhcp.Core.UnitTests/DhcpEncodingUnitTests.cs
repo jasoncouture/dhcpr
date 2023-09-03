92ce434f19767838569b71fcaf5b3c5943c6e256
Jason Couture <jasonc@alertr.info>
2023-09-03 14:51:45 -0400

    Add unit test for DHCP Message Encoding

---
+++ Dhcpr.Dhcp.Core.UnitTests/DhcpEncodingUnitTests.cs
@@ -0,0 +1,25 @@
+using System.Collections.Immutable;
+using System.Net;
+using System.Security.Cryptography;
+
+using Dhcpr.Dhcp.Core.Protocol;
+
+namespace Dhcpr.Dhcp.Core.UnitTests;
+
+public class DhcpEncodingUnitTests
+{
+    [Fact]
+    public void DhcpMessageCanDecodeItsOwnEncodedMessage()
+    {
+        var testMessage = new DhcpMessage(BootOperationCode.Request, HardwareAddressType.Ethernet, 6, 0, 1234, 1,
+            DhcpFlags.Broadcast, IPAddress.Any, IPAddress.Any, IPAddress.Any, IPAddress.Any,
+            new HardwareAddress(Enumerable.Range(0, 16).Select(i => (byte)i).ToImmutableArray(), 6), "abcd", "efg",
+            new DhcpOptionCollection(ImmutableArray<DhcpOption>.Empty));
+
+        Span<byte> testMessageBytes = stackalloc byte[testMessage.Size];
+        testMessage.EncodeTo(testMessageBytes);
+        Assert.True(DhcpMessage.TryParse(testMessageBytes, out var decodedMessage));
+        Assert.NotNull(decodedMessage);
+        Assert.Equal(testMessage.HardwareAddressType, decodedMessage.HardwareAddressType);
+    }
+}

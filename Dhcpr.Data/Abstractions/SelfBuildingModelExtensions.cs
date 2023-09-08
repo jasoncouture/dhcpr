ecfbf2daf8bbef0a707cfd7c6f6ef83207b36197
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Add extension method to make it easier to setup types in model builder.

---
+++ Dhcpr.Data/Abstractions/SelfBuildingModelExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dhcpr.Data.Abstractions;
+
+static class SelfBuildingModelExtensions
+{
+    public static void OnModelCreating<T>(this ModelBuilder builder) where T : class, ISelfBuildingModel<T>
+    {
+        T.OnModelCreating(builder.Entity<T>());
+    }
+}

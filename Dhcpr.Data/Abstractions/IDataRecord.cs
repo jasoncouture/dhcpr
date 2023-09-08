f277d035a225e24fd4ea6c77630d93b6588460f7
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Create interfaces to abstract common fields on DB tables

---
+++ Dhcpr.Data/Abstractions/IDataRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace Dhcpr.Data.Abstractions;
+
+internal interface IDataRecord<T> : ISelfBuildingModel<T> where T : class, IDataRecord<T>
+{
+    string Id { get; set; }
+    DateTimeOffset Created { get; set; }
+    DateTimeOffset Modified { get; set; }
+}
---
+++ Dhcpr.Data/Abstractions/ISelfBuildingModel.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dhcpr.Data.Abstractions;
+
+internal interface ISelfBuildingModel<T> where T : class, ISelfBuildingModel<T>
+{
+    abstract static void OnModelCreating(EntityTypeBuilder<T> builder);
+}

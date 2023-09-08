d4ea41eae51bde0910ba48d1a1714f6d8bde027f
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Create static class to keep DNS model configuration away from the main context

---
+++ Dhcpr.Data/Dns/DnsDataModel.cs
@@ -0,0 +1,28 @@
+﻿using Dhcpr.Data.Abstractions;
+using Dhcpr.Data.Dns.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Dhcpr.Data.Dns;
+
+static class DnsDataModel
+{
+    private static void OnModelCreating<T>(ModelBuilder builder) where T : class, ISelfBuildingModel<T> =>
+        builder.OnModelCreating<T>();
+
+    public static void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreating<DnsNameRecord>(modelBuilder);
+        OnModelCreating<DnsResourceRecord>(modelBuilder);
+
+        OnModelCreating<NameServerDnsResourceRecord>(modelBuilder);
+        OnModelCreating<CanonicalNameDnsResourceRecord>(modelBuilder);
+        OnModelCreating<MailExchangerDnsResourceRecord>(modelBuilder);
+        OnModelCreating<TextDnsResourceRecord>(modelBuilder);
+        OnModelCreating<ServiceDnsResourceRecord>(modelBuilder);
+        OnModelCreating<PointerDnsResourceRecord>(modelBuilder);
+        OnModelCreating<InterNetworkVersion4AddressResourceRecord>(modelBuilder);
+        OnModelCreating<InterNetworkVersion6AddressDnsResourceRecord>(modelBuilder);
+        OnModelCreating<StartOfAuthorityDnsResourceRecord>(modelBuilder);
+    }
+}

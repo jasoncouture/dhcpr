d558d6642a22219d49e2a5da0f59794b08240057
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:14 -0400

    Add stub for DHCP DI Integration

--- Dhcpr.Dhcp.Core/DhcpConfiguration.cs
+++ Dhcpr.Dhcp.Core/DhcpConfiguration.cs
@@ -1,5 +1,8 @@
 ﻿using Dhcpr.Core;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Dhcpr.Dhcp.Core;
 
 public sealed class DhcpConfiguration : IValidateSelf
@@ -8,5 +11,14 @@ public sealed class DhcpConfiguration : IValidateSelf
     public bool Validate()
     {
         throw new NotImplementedException();
+    }
+}
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDhcp(this IServiceCollection services, IConfiguration configuration)
+    {
+        // TODO: Add DHCP components and configuration.
+        return services;
     }
 }

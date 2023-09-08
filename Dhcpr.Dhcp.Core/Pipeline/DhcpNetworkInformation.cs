1e72fe72945678cb507e04f54eef58912771d31a
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Move types into matching files

---
+++ Dhcpr.Dhcp.Core/Pipeline/DhcpNetworkInformation.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Dhcpr.Dhcp.Core.Pipeline;
+
+public record DhcpNetworkInformation(
+    IPAddress LocalAddress,
+    IPAddress NetworkMask,
+    IPAddress BroadcastAddress,
+    int InterfaceIndex
+);
---
+++ Dhcpr.Dhcp.Core/Pipeline/DhcpRequestContext.cs
@@ -0,0 +1,10 @@
+﻿using Dhcpr.Dhcp.Core.Protocol;
+
+namespace Dhcpr.Dhcp.Core.Pipeline;
+
+public class DhcpRequestContext
+{
+    public required DhcpNetworkInformation NetworkInformation { get; init; }
+    public required DhcpMessage Message { get; init; }
+    public DhcpMessage? Response { get; init; }
+}

f6fa3d4923d99ee4b820f7ebff793c31399a0d2b
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Provide accurate TTLs to clients.
    By subtracting the time in cache from the TTL, we give the client a more accurate TTL. We also don't need to worry much about flooding when it expires, since it's granular to the second, so different clients will expire it at different times, basically jittering it for us.

--- Dhcpr.Dns.Core/Resolvers/Caching/DnsCache.cs
+++ Dhcpr.Dns.Core/Resolvers/Caching/DnsCache.cs
@@ -3,6 +3,7 @@
 using Dhcpr.Core.Linq;
 
 using DNS.Protocol;
+using DNS.Protocol.ResourceRecords;
 
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
@@ -27,23 +28,65 @@ public class DnsCache : IDnsCache
         var key = new QueryCacheKey(request, request.Questions[0]);
         if (_memoryCache.TryGetValue(key, out QueryCacheData? data) && data is not null)
         {
-            response = new Response(data.Response);
+            response = WithUpdatedTimeToLive(data.Response, DateTimeOffset.Now - data.Created);
+            if (
+                response.AuthorityRecords.Any(i => i.TimeToLive <= TimeSpan.Zero) ||
+                response.AnswerRecords.Any(i => i.TimeToLive <= TimeSpan.Zero) ||
+                response.AdditionalRecords.Any(i => i.TimeToLive <= TimeSpan.Zero)
+                )
+            {
+                // This is expired, but somehow still in cache. Evict it and lie that we didn't find it.
+                _memoryCache.Remove(key);
+                return false;
+            }
             response.Id = request.Id;
-            response.AuthorativeServer = false;
-            // TODO: Update answers with adjusted TTL
             return true;
         }
 
         return false;
     }
 
+    private IResponse WithUpdatedTimeToLive(IResponse response, TimeSpan currentAge)
+    {
+        using var additionalRecords = response.AdditionalRecords.ToPooledList();
+        using var answerRecords = response.AnswerRecords.ToPooledList();
+        using var authorityRecords = response.AuthorityRecords.ToPooledList();
+        response = new Response(response);
+        response.AnswerRecords.Clear();
+        response.AdditionalRecords.Clear();
+        response.AuthorityRecords.Clear();
+
+        foreach (var item in answerRecords)
+            response.AnswerRecords.Add(WithUpdatedTimeToLive(item, currentAge));
+
+        foreach (var item in additionalRecords)
+            response.AdditionalRecords.Add(WithUpdatedTimeToLive(item, currentAge));
+
+        foreach (var item in authorityRecords)
+            response.AuthorityRecords.Add(WithUpdatedTimeToLive(item, currentAge));
+
+        return response;
+    }
+
+    private IResourceRecord WithUpdatedTimeToLive(IResourceRecord resourceRecord, TimeSpan currentAge)
+    {
+        var ttl = resourceRecord.TimeToLive - currentAge;
+        if (ttl < TimeSpan.Zero) ttl = TimeSpan.FromSeconds(0);
+        // We re-create this this way, because something may want to use a specific record type to detect things
+        // And we don't want to break that.
+        var recordBytes = new ResourceRecord(resourceRecord.Name, resourceRecord.Data, resourceRecord.Type, resourceRecord.Class, ttl).ToArray();
+        return ResourceRecordFactory.FromArray(recordBytes, 0);
+    }
+
     public void TryAddCacheEntry(IRequest request, IResponse response)
     {
         if (request.Questions.Count != 1) return;
-        if (request.Questions[0].Type is RecordType.A or RecordType.AAAA && response.ResponseCode == ResponseCode.NoError && response.AnswerRecords.Count == 0)
+        if (request.Questions[0].Type is RecordType.A or RecordType.AAAA &&
+            response.ResponseCode == ResponseCode.NoError && response.AnswerRecords.Count == 0)
         {
             return;
         }
+
         using var timeToLivePooledList =
             response.AnswerRecords.Concat(response.AdditionalRecords).Concat(response.AuthorityRecords)
                 .Select(i => i.TimeToLive)
@@ -53,6 +96,7 @@ public class DnsCache : IDnsCache
         {
             timeToLivePooledList.Add(TimeSpan.FromMinutes(1));
         }
+
         var cacheTimeToLive = timeToLivePooledList.First();
         if (cacheTimeToLive <= TimeSpan.Zero) return;
 
@@ -69,6 +113,7 @@ public class DnsCache : IDnsCache
         {
             cacheEntry.SlidingExpiration = cacheSlidingExpiration;
         }
+
         cacheEntry.Priority = CacheItemPriority.High;
         cacheEntry.RegisterPostEvictionCallback(DnsCacheEntryEvictedCallback);
         cacheEntry.Dispose();

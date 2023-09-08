4b32f10dbd2187a17a7c6f80d1cfec0a17a1fa79
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Add value generators to maintain automatic values

---
+++ Dhcpr.Data/ValueGenerators/StringIdValueGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace Dhcpr.Data.ValueGenerators;
+
+public class StringIdValueGenerator : ValueGenerator<string>
+{
+    private readonly IProperty _property;
+
+    public StringIdValueGenerator(IProperty property)
+    {
+        _property = property;
+    }
+
+    public override string Next(EntityEntry entry)
+    {
+        var currentValue = entry.Property(_property).CurrentValue as string;
+        if (!string.IsNullOrWhiteSpace(currentValue)) return currentValue;
+        if (entry.State != EntityState.Added) return currentValue!;
+        currentValue = Guid.NewGuid().ToString("n").Substring(0, 8);
+        return currentValue;
+    }
+
+    public override bool GeneratesTemporaryValues => false;
+}

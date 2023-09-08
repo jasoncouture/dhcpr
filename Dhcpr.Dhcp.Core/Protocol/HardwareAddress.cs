9fd2243e7296c1cbf456b2dd3187599be4dd773b
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:14 -0400

    Add methods for hardware address to parse itself.

--- Dhcpr.Dhcp.Core/Protocol/HardwareAddress.cs
+++ Dhcpr.Dhcp.Core/Protocol/HardwareAddress.cs
@@ -1,12 +1,36 @@
-﻿using System.Collections.Immutable;
+﻿using System.Buffers;
+using System.Collections.Immutable;
 
 namespace Dhcpr.Dhcp.Core.Protocol;
 
 public sealed record HardwareAddress(ImmutableArray<byte> Bytes, int Length)
 {
+    internal const int HardwareAddressMaxLength = 16;
+
+    public static HardwareAddress ReadAndAdvance(ref ReadOnlySpan<byte> bytes, int length)
+    {
+        var ret = new HardwareAddress(bytes[..HardwareAddressMaxLength].ToImmutableArray(), length);
+        bytes = bytes[HardwareAddressMaxLength..];
+        return ret;
+    }
     public override string ToString()
     {
-        var slicedBytes = Bytes[..Length].ToArray();
-        return BitConverter.ToString(slicedBytes).Replace('-', ':').ToUpper();
+        var array = ArrayPool<byte>.Shared.Rent(Length);
+        try
+        {
+            Bytes.CopyTo(0, array, 0, Length);
+            return BitConverter.ToString(array, 0, Length).Replace('-', ':').ToUpper();
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(array);
+        }
+    }
+
+    public void WriteAndAdvance(ref Span<byte> buffer)
+    {
+        var toCopy = Math.Min(Math.Min(Length, buffer.Length), HardwareAddressMaxLength);
+        Bytes[..toCopy].CopyTo(buffer);
+        buffer = buffer[16..];
     }
 }

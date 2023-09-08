4d071ecab1354af23d923b9754bf356756e7c65b
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:14 -0400

    Move types to their own files

---
+++ Dhcpr.Dhcp.Core/SubnetConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.NetworkInformation;
+
+using Dhcpr.Core;
+
+namespace Dhcpr.Dhcp.Core;
+
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public sealed class SubnetConfiguration : IValidateSelf
+{
+    public required string CIDR { get; set; }
+    public int TimeToLiveSeconds { get; set; }
+    public bool Enabled { get; set; }
+    public bool Validate()
+    {
+        if (TimeToLiveSeconds < 300) return false;
+        if (!CIDR.TryParseClasslessInterDomainRouting(out var cidrAddress, out var cidrNetmask))
+            return false;
+        try
+        {
+            var foundNetworkAddress = NetworkInterface.GetAllNetworkInterfaces()
+                .Any(i =>
+                    i.GetIPProperties()
+                        .UnicastAddresses
+                        .Any(x => x.Address.IsInNetwork(cidrAddress, cidrNetmask)
+                        ));
+            if (!foundNetworkAddress) return false;
+        }
+        catch
+        {
+            // Assume it's valid, and we just don't have permission.
+            // We'll find out soon enough when we try to bind interfaces.
+        }
+
+        return true;
+    }
+}

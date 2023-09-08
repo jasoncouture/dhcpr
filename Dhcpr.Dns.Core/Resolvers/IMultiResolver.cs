1de911550416db197252f4d30cb51941ac7f088c
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:14 -0400

    Recursive resolution now works against root servers.
    This also adds some initial caching, DNS caching is next.

---
+++ Dhcpr.Dns.Core/Resolvers/IMultiResolver.cs
@@ -0,0 +1,11 @@
+﻿using DNS.Client.RequestResolver;
+
+namespace Dhcpr.Dns.Core;
+
+public interface IMultiResolver : IRequestResolver
+{
+    void ReplaceResolvers(params IRequestResolver[] resolvers);
+    void AddResolvers(params IRequestResolver[] resolvers);
+    void AddResolver(IRequestResolver resolver);
+    void RemoveResolver(IRequestResolver resolver);
+}

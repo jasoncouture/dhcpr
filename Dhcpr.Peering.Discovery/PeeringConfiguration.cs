da589791fa27484690e1e38c313d16b7c53ab24b
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:14 -0400

    Add top level peering configuration

---
+++ Dhcpr.Peering.Discovery/PeeringConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Dhcpr.Core;
+
+namespace Dhcpr.Peering.Discovery;
+
+public class PeeringConfiguration : IValidateSelf
+{
+    public int PeerCommunicationPort { get; set; } = PeeringConstants.DefaultClusterPort;
+    public string[] PeerSubnets { get; set; } = Array.Empty<string>();
+    public PeerDiscoveryMethod PeeringMethod { get; set; } = PeerDiscoveryMethod.None;
+
+    public bool Validate()
+    {
+        if (PeerCommunicationPort > 65535 || PeerCommunicationPort <= 0)
+            return false;
+
+        return PeerSubnets.All(subnet => subnet.TryParseClasslessInterDomainRouting(out _, out _));
+    }
+}

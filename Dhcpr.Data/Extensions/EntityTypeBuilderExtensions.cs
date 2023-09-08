b9022a6200147f21bea9a7b18902770df670d6bd
Jason Couture <jasonc@alertr.info>
2023-09-08 01:01:15 -0400

    Create extension method to add generators where necessary.

---
+++ Dhcpr.Data/Extensions/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Dhcpr.Data.Abstractions;
+using Dhcpr.Data.ValueGenerators;
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dhcpr.Data.Extensions;
+
+public static class EntityTypeBuilderExtensions
+{
+    internal static void AddDataRecordValueGenerators<T>(this EntityTypeBuilder<T> builder)
+        where T : class, IDataRecord<T>
+    {
+        builder.Property(i => i.Id).HasValueGenerator<StringIdValueGenerator>()
+            .HasValueGenerator((p, e) => new StringIdValueGenerator(p));
+        builder.Property(i => i.Modified)
+            .HasValueGenerator((p, e) =>
+                new DateTimeOffsetValueGenerator(p, generateOnAdd: true, generateOnModify: true));
+        builder.Property(i => i.Created)
+            .HasValueGenerator((p, e) =>
+                new DateTimeOffsetValueGenerator(p, generateOnAdd: true, generateOnModify: false));
+    }
+}

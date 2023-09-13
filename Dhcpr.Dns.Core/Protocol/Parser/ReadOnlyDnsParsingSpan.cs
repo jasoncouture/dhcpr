4f824bdec9e25a53513a747c8ae28fe8ece141bf
Jason Couture <jasonc@alertr.info>
2023-09-13 18:16:41 -0400

    WIP

--- Dhcpr.Dns.Core/Protocol/Parser/ReadOnlyDnsParsingSpan.cs
+++ Dhcpr.Dns.Core/Protocol/Parser/ReadOnlyDnsParsingSpan.cs
@@ -1,4 +1,8 @@
-﻿namespace Dhcpr.Dns.Core.Protocol.Parser;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Dhcpr.Core.Linq;
+
+namespace Dhcpr.Dns.Core.Protocol.Parser;
 
 public ref struct ReadOnlyDnsParsingSpan
 {
@@ -47,4 +51,111 @@ public ref struct ReadOnlyDnsParsingSpan
 
     public ReadOnlyDnsParsingSpan FromStart() => new(_start);
     public int Offset => _offset;
+}
+
+public class LabelTree<TValue>
+{
+    private LabelTreeEntry<TValue> _root = new()
+    {
+        Label = string.Empty
+    };
+    public bool TryAdd(string[] keyPath, TValue value)
+    {
+        LabelTreeEntry<TValue>? nextChild = _root;
+        // TODO
+        throw new NotImplementedException();
+    }
+}
+
+[SuppressMessage("ReSharper", "StaticMemberInGenericType")]
+public class LabelTreeEntry<TValue> : IComparable<string>, IDisposable
+{
+    public required string Label { get; init; } = string.Empty;
+    public TValue Value { get; init; } = default!;
+
+    private PooledList<LabelTreeEntry<TValue>> _children = ListPool<LabelTreeEntry<TValue>>.Default.Get();
+    private PooledList<string> _childrenKeys = ListPool<string>.Default.Get();
+
+
+    public int CompareTo(string? other)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(other);
+        return string.Compare(Label, other, StringComparison.OrdinalIgnoreCase);
+    }
+
+    private int GetIndex(string key)
+    {
+        var index = _childrenKeys.BinarySearch(key, StringComparer.OrdinalIgnoreCase);
+        return index;
+    }
+    
+    public bool TryAdd(string key, TValue value)
+    {
+        var index = GetIndex(key);
+        if (index >= 0)
+            return false;
+        var insertIndex = ~index;
+        _childrenKeys.Insert(insertIndex, key);
+        _children.Insert(insertIndex, new LabelTreeEntry<TValue>() { Label = key, Value = value });
+        return true;
+    }
+
+    private static readonly ThreadLocal<Stack<LabelTreeEntry<TValue>>> PooledChildStacks =
+        new(() => new Stack<LabelTreeEntry<TValue>>());
+
+    private static readonly ThreadLocal<Stack<string>> PooledLabelStacks = new(() => new Stack<string>());
+
+    public bool TryGetValue(IList<string> labels, [MaybeNullWhen(false)] out TValue value)
+    {
+        value = default;
+        if (!TryGetChild(labels, out var child))
+            return false;
+
+        value = child.Value;
+
+        return true;
+    }
+
+    internal bool TryGetChild(IList<string> keyPath, [MaybeNullWhen(false)] out LabelTreeEntry<TValue> child)
+    {
+        child = default;
+        LabelTreeEntry<TValue>? nextChild = this;
+        for (var x = 1; x <= keyPath.Count; x++)
+        {
+            var nextLabel = keyPath[^x];
+
+            if (!nextChild.TryGetChild(nextLabel, out nextChild))
+                return false;
+        }
+
+        child = nextChild;
+        return true;
+    }
+
+    internal bool TryGetChild(string key, [MaybeNullWhen(false)] out LabelTreeEntry<TValue> child)
+    {
+        child = default;
+        var index = GetIndex(key);
+        if (index < 0)
+            return false;
+
+        child = _children[index];
+        return true;
+    }
+    public bool TryGetValue(string key, [MaybeNullWhen(false)] out TValue value)
+    {
+        var ret = TryGetChild(key, out var child);
+        value = child switch
+        {
+            not null => child.Value,
+            _ => default(TValue)
+        };
+        return ret;
+    }
+
+    public void Dispose()
+    {
+        _children.Dispose();
+        GC.SuppressFinalize(this);
+    }
 }

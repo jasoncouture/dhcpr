d4cf20d54f3c29394c93ae0109e614e3b3b0aeb0
Jason Couture <jasonc@alertr.info>
2026-07-22 09:03:57 -0400

    feat: Add script to spam the server slightly to test performance

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dig @127.0.0.1 -p 65353 fast.com
+dig @127.0.0.1 -p 65353 www.google.com
+dig @127.0.0.1 -p 65353 www.facebook.com
+dig @127.0.0.1 -p 65353 telecom-transcription-service.wus2-01.telecom.devtest.telecom.srv.st.dev
